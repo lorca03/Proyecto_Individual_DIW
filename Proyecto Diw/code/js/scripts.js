c095e09ff3f2fc54494e5f898935608c26410a2f
lorca03 <guillermolorcamartinez@gmail.com>
2022-11-04 13:38:47 +0100

    pop up

--- Proyecto Diw/code/js/scripts.js
+++ Proyecto Diw/code/js/scripts.js
@@ -4,10 +4,21 @@ function subirArriba(e) {
   document.documentElement.scrollTop=0
 }
 window.onscroll = function () {
-  if (document.documentElement.scrollTop > 200) {
+  if (document.documentElement.scrollTop > 400) {
     document.querySelector(".irArriba").style.display = "block";
     goUp.addEventListener("click", subirArriba);
   } else {
     document.querySelector(".irArriba").style.display = "none";
   }
 };
+
+/*pop up */
+const btnCerrarModal=document.getElementById('cerrar-modal');
+const modal=document.getElementById('modal');
+
+function abrirModal(){
+  modal.showModal();
+}
+btnCerrarModal.addEventListener('click',()=>{
+  modal.close();
+})
